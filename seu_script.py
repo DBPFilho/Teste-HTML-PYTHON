ff7bad897916ae3af02031245a8903dfa517ae42
DBPFilho <150073664+DBPFilho@users.noreply.github.com>
2024-05-04 17:03:17 -0300

    Update seu_script.py

--- seu_script.py
+++ seu_script.py
@@ -27,5 +27,7 @@ def index():
     return render_template('index.html', resultado=resultado)
 
 if __name__ == '__main__':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
     app.run(debug=True)
 
